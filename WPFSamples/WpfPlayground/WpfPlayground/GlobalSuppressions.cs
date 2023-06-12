2ede8747b91e2da4fd57d529c67714fe0dd95d39
John-Michael Wiley <jmwiley@outlook.com>
2023-06-12 11:04:35 -0400

    Update code to suppress errors and also link back to the Nuget package for JMWToolkit.MVVM.

--- WPFSamples/WpfPlayground/WpfPlayground/GlobalSuppressions.cs
+++ WPFSamples/WpfPlayground/WpfPlayground/GlobalSuppressions.cs
@@ -5,10 +5,7 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~P:WpfPlayground.ViewModels.MainWindowViewModel.SelectedItem")]
-[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:WpfPlayground.Dialogs.AddItemDialog.OnSourceInitialized(System.EventArgs)")]
-[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~P:WpfPlayground.ViewModels.MessageBoxDemoAreaViewModel.ImageInfos")]
-[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:WpfPlayground.ViewModels.MessageBoxDemoAreaViewModel.#ctor")]
-[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~P:WpfPlayground.ViewModels.MessageBoxDemoAreaViewModel.MessageBoxButton")]
-[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:WpfPlayground.ViewModels.IconExtensions.ToImageSource(System.Drawing.Icon)~System.Windows.Media.ImageSource")]
-[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:WpfPlayground.ViewModels.ButtonCheckBoxConverter.Convert(System.Object,System.Type,System.Object,System.Globalization.CultureInfo)~System.Object")]
+[assembly: SuppressMessage(
+    "Interoperability", "CA1416:Validate platform compatibility", 
+    Justification = "Not going to support older platforms", 
+    Scope = "module")]
